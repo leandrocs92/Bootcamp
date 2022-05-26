bf807b75c781b6ed22c50de0d5a9989cbc370c28
leandrocs92 <leandrosilveira92@hotmail.com>
2022-05-26 19:51:33 -0300

    atualizações

--- JavaScript/Sintax e Operadores/assets/js/script.js
+++ JavaScript/Sintax e Operadores/assets/js/script.js
@@ -1,8 +1,8 @@
 var num1 = Number(prompt("Digite o primeiro valor"));
 var num2 = Number(prompt("Digite o segundo valor"));
-var soma = num1 + num2;
+const soma = num1 + num2;
 
-if (num1 == num2) {
+if (num1 === num2) {
     if (soma < 10) {
         if (soma < 20) {
             alert(`Os números ${num1} e ${num2} são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20`);
