a19abf6452ab07111432252f54680c7859f244fc
leandrocs92 <leandrosilveira92@hotmail.com>
2022-06-01 20:39:15 -0300

    Orientação a Objetos
    Criação de modelo simples de conta bancária.

---
+++ JavaScript/Orientação a Objetos/scripts.js
@@ -0,0 +1,70 @@
+class ContaBancaria {
+    constructor(agencia, numero, tipo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    sacar(valor) {
+        if (this._saldo < valor) {
+            return "Operação inválida!"
+        }
+
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+
+    depositar(valor) {
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends ContaBancaria {
+    constructor(agencia, numero, cartaoCredito) {
+        super(agencia, numero);
+        this.tipo = 'Corrente';
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get() {
+        return this._cartaoCredito;
+    }
+
+    set(valor) {
+        this._cartaoCredito = valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'Poupança';
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'Universitária';
+    }
+
+    saque(valor) {
+        if (valor >= 500) return "Operação inválida!!"
+
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+}
